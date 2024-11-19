8354a903ed7d872fda42d8f3f4efdb7ec964b557
Gabriel Florentino <gabriel.florentino@discente.ufg.br>
2024-11-19 16:32:29 -0300

    Criando uma struct para inserir canditados

---
+++ Urna.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Função responsávl por gereciar informações a cerca do canditado
+typedef struct
+{
+  char nomeCanditado[100];
+  int numCanditado;
+  int numVotos;
+} Canditado;
+
+void cadastrarCanditados(Canditado Canditado[], int n)
+{
+
+  for (int i = 0; i < n; i++)
+  {
+    printf("\n Cadastre o Canditado %d:\n ", i + 1);
+    printf("Nome do Candditado: ");
+    scanf("%c");
+    fgets(Canditado[i].nomeCanditado, 100, stdin);
+    candidatos[i].nomeCanditado[strcspn(candidatos[i].nome, "\n")] = 0;
+    printf("Numero do Candidato: ");
+    scanf("%d", &candidatos[i].numeroCandito);
+    candidatos[i].numVotos = 0;
+  }
+}
